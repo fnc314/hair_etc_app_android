cc386a9c338d044aadab7929ea8b9af55e02e17f
Franco <fnc314@gmail.com>
2014-04-09 15:51:17 -0700

    removed WebView and added button to launch a form for creating new appointments

---
+++ src/com/hairetc/hairetcapp1/Form.java
@@ -0,0 +1,15 @@
+package com.hairetc.hairetcapp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Form extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.form);
+	}
+	
+}
--- src/com/hairetc/hairetcapp1/NewForm.java
+++
@@ -1,7 +0,0 @@
-package com.hairetc.hairetcapp1;
-
-import android.app.Activity;
-
-public class NewForm extends Activity {
-
-}
