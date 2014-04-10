55d939da3d3c975bd341fd7c63d2b219b393b506
Franco <fnc314@gmail.com>
2014-04-10 04:17:58 -0700

    small styling changes and sliding form down farther on screen, need to implement scroll view for smaller devices and tackle landscape properly, rethink use of cb and apptSubmit in Form.java, need to change Toast to Twilio call or email/text

--- src/com/hairetc/hairetcapp1/Form.java
+++ src/com/hairetc/hairetcapp1/Form.java
@@ -28,10 +28,10 @@ public class Form extends Activity implements OnItemSelectedListener {
 	private EditText apptTime;
 	private Button apptSubmit;
 	private Spinner spStylistSelect;
-	private CheckBox cbHairCut, cbPerm, cbPartialHilites, cbHilites, cbColor, 
-	cbPartialColor, cbUpdo, cbBrazilianBlowout, cbWax, cbExtensions;
+	private CheckBox cbHairCut, cbExtensions, cbColor, cbPartialColor, 
+		cbHilites, cbPartialHilites, cbPerm, cbUpdo, cbBrazilianBlowout, cbWax;
 	private String chosenStylist;
-	private String chosenServices;
+	private String chosenServices = "";
 	private String chosenDate;
 	private String chosenTime;
 	private String toastString;
@@ -62,7 +62,7 @@ public class Form extends Activity implements OnItemSelectedListener {
 	    }
 	};
 	
-	// Create TimePickerDialogue that picks and shows time in associated text view
+	// TASK: Create TimePickerDialogue that picks and shows time in associated text view
 	public void timePick(View v) {
 		new TimePickerDialog(Form.this, time, 
 				c.get(Calendar.HOUR), c.get(Calendar.MINUTE), false).show();
@@ -91,11 +91,13 @@ public class Form extends Activity implements OnItemSelectedListener {
 	}
 	
 	// Abstracted out functionality to avoid repetition
-	// Checkboxes -> Services
+	// Checkboxes -> Services (necessary?)
+	// Do not use any cb nor apptSubmit
 	public void findViews() {
 		apptDate = (EditText)findViewById(R.id.apptDate);
 		apptTime = (EditText)findViewById(R.id.apptTime);
 		spStylistSelect = (Spinner)findViewById(R.id.spStylistSelect);
+		apptSubmit = (Button)findViewById(R.id.apptSubmit);
 		cbHairCut = (CheckBox)findViewById(R.id.cbHairCut);
 		cbPerm = (CheckBox)findViewById(R.id.cbPerm);
 		cbPartialHilites = (CheckBox)findViewById(R.id.cbPartialHilites);
@@ -120,8 +122,14 @@ public class Form extends Activity implements OnItemSelectedListener {
 	// Make temporary Toast to show customers what they chose
 	
 	public void tempToast() {
-		toastString = chosenStylist + " on " + chosenDate + " at " + chosenTime;
-		Toast toast = Toast.makeText(this, toastString, Toast.LENGTH_SHORT);
+		toastString = chosenServices 
+				+ "with " 
+				+ chosenStylist 
+				+ " on " 
+				+ chosenDate 
+				+ " at " 
+				+ chosenTime;
+		Toast toast = Toast.makeText(this, toastString, Toast.LENGTH_LONG);
 		toast.show();
 	}
 	
@@ -136,22 +144,22 @@ public class Form extends Activity implements OnItemSelectedListener {
 		int stylistId = spStylistSelect.getSelectedItemPosition();
 		switch (stylistId) {
 		case 0:
-			chosenStylist = "Theresa";
+			chosenStylist = getString(R.string.theresa);
 			break;
 		case 1:
-			chosenStylist = "Chrissy";
+			chosenStylist = getString(R.string.chrissy);
 			break;
 		case 2:
-			chosenStylist = "Stacy";
+			chosenStylist = getString(R.string.stacy);
 			break;
 		case 3:
-			chosenStylist = "Sheri";
+			chosenStylist = getString(R.string.sheri);
 			break;
 		case 4:
-			chosenStylist = "Krista";
+			chosenStylist = getString(R.string.krista);
 			break;
 		case 5:
-			chosenStylist = "Junae";
+			chosenStylist = getString(R.string.junae);
 			break;
 		}
 		
@@ -161,10 +169,99 @@ public class Form extends Activity implements OnItemSelectedListener {
 	@Override
 	public void onNothingSelected(AdapterView<?> arg0) {
 		// TODO Auto-generated method stub
-		
+		chosenStylist = "Theresa";
 	}
-
-	  
+	
+	// Code implemented directly from http://developer.android.com/guide/topics/ui/controls/checkbox.html
+	public void selectedServices(View v) {
+		// Status of checkbox
+		// true = checked false = unchecked
+		boolean checked = ( ( CheckBox ) v ).isChecked();
+		
+		// Determine which check boxes were checked
+		switch ( v.getId() ) {
+		case R.id.cbHairCut:
+			if (checked) {
+				chosenServices += getString(R.string.haircut) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.haircut) + " "), "");
+				break;
+			}
+		case R.id.cbExtensions:
+			if (checked) {
+				chosenServices += getString(R.string.hairExtensions) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.hairExtensions) + " "), "");
+				break;
+			}
+		case R.id.cbColor:
+			if (checked) {
+				chosenServices += getString(R.string.color) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.color) + " "), "");
+				break;
+			}
+		case R.id.cbPartialColor:
+			if (checked) {
+				chosenServices += getString(R.string.partialColor) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.partialColor) + " "), "");
+				break;
+			}
+		case R.id.cbHilites:
+			if (checked) {
+				chosenServices += getString(R.string.hilites) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.hilites) + " "), "");
+				break;
+			}
+		case R.id.cbPartialHilites:
+			if (checked) {
+				chosenServices += getString(R.string.partialHilites) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.partialHilites) + " "), "");
+				break;
+			}
+		case R.id.cbPerm:
+			if (checked) {
+				chosenServices += getString(R.string.perm) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.perm) + " "), "");
+				break;
+			}
+		case R.id.cbUpdo:
+			if (checked) {
+				chosenServices += getString(R.string.updo) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.updo) + " "), "");
+				break;
+			}
+		case R.id.cbBrazilianBlowout:
+			if (checked) {
+				chosenServices += getString(R.string.brazilianBlowout) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.brazilianBlowout)+ " "), "");
+				break;
+			}
+		case R.id.cbWax:
+			if (checked) {
+				chosenServices += getString(R.string.wax) + " ";
+				break;
+			} else {
+				chosenServices = chosenServices.replaceAll((getString(R.string.wax) + " "), "");
+				break;
+			}
+		}
+	}
 	// Create Method That Sends email (built in separate class? -> passing values? (maybe as integers and strings))
 	// Confirm with TOAST and redirect to main activity
 	
