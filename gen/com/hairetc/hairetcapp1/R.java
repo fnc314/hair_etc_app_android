d638c04561b7fad608c61ea29fcc944db10f9fe3
Franco <fnc314@gmail.com>
2014-04-10 01:05:50 -0700

    reworked form, added spinner, use edittext fields to kick off dialog pop up, toast generated with date, time, and stylist upon submit

--- gen/com/hairetc/hairetcapp1/R.java
+++ gen/com/hairetc/hairetcapp1/R.java
@@ -16,6 +16,9 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
+    public static final class array {
+        public static final int stylists=0x7f0c0000;
+    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -996,13 +999,23 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f050051;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
-        public static final int apptDate=0x7f050040;
-        public static final int apptSubmit=0x7f050042;
-        public static final int apptTime=0x7f050041;
+        public static final int apptDate=0x7f05004e;
+        public static final int apptSubmit=0x7f050050;
+        public static final int apptTime=0x7f05004f;
         public static final int beginning=0x7f050011;
+        public static final int cbBrazilianBlowout=0x7f05004c;
+        public static final int cbColor=0x7f050043;
+        public static final int cbExtensions=0x7f050041;
+        public static final int cbHairCut=0x7f050040;
+        public static final int cbHilites=0x7f050046;
+        public static final int cbPartialColor=0x7f050044;
+        public static final int cbPartialHilites=0x7f050047;
+        public static final int cbPerm=0x7f050049;
+        public static final int cbUpdo=0x7f05004a;
+        public static final int cbWax=0x7f05004d;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
@@ -1043,11 +1056,15 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int spStylistSelect=0x7f05003e;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003e;
-        public static final int textView2=0x7f05003f;
+        public static final int tableRow1=0x7f05003f;
+        public static final int tableRow2=0x7f050042;
+        public static final int tableRow3=0x7f050045;
+        public static final int tableRow4=0x7f050048;
+        public static final int tableRow5=0x7f05004b;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1110,7 +1127,7 @@ containing a value of this type.
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int main=0x7f0d0000;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1153,17 +1170,23 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int button=0x7f0a0010;
-        public static final int checkbox=0x7f0a0012;
-        public static final int datePicked=0x7f0a0016;
-        public static final int datepickbutton=0x7f0a0013;
-        public static final int hello_world=0x7f0a000e;
-        public static final int submitbutton=0x7f0a0015;
-        public static final int test=0x7f0a0011;
-        public static final int timePicked=0x7f0a0017;
-        public static final int timepickbutton=0x7f0a0014;
+        public static final int blowout=0x7f0a001a;
+        public static final int button=0x7f0a000f;
+        public static final int color=0x7f0a0014;
+        public static final int datePicked=0x7f0a0011;
+        public static final int hairExtensions=0x7f0a001b;
+        public static final int haircut=0x7f0a0013;
+        public static final int highlites=0x7f0a0016;
+        public static final int partialColor=0x7f0a0015;
+        public static final int partialHighlites=0x7f0a0017;
+        public static final int perm=0x7f0a0018;
+        public static final int spPromptStylist=0x7f0a001d;
+        public static final int submitbutton=0x7f0a0010;
+        public static final int timePicked=0x7f0a0012;
+        public static final int updo=0x7f0a0019;
+        public static final int wax=0x7f0a001c;
     }
     public static final class style {
         /** 
--- src/com/hairetc/hairetcapp1/Form.java
+++ src/com/hairetc/hairetcapp1/Form.java
@@ -9,18 +9,32 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class Form extends Activity {
+public class Form extends Activity implements OnItemSelectedListener {
 	
 	// Global variables for getting date and time information
 	final Calendar c = Calendar.getInstance();
 	private EditText apptDate;
 	private EditText apptTime;
 	private Button apptSubmit;
+	private Spinner spStylistSelect;
+	private CheckBox cbHairCut, cbPerm, cbPartialHilites, cbHilites, cbColor, 
+	cbPartialColor, cbUpdo, cbBrazilianBlowout, cbWax, cbExtensions;
+	private String chosenStylist;
+	private String chosenServices;
+	private String chosenDate;
+	private String chosenTime;
+	private String toastString;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,8 @@ public class Form extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.form);
 		findViews();
+		populateSpinnerWithStylists();
+
 	}
 	
 	// Create DatePickerDialogue that picks and shows date in associated text view
@@ -65,24 +81,94 @@ public class Form extends Activity {
 	public void showSelected() {
 		String dateFormat = "MM-dd-yyyy";
 		SimpleDateFormat sdf = new SimpleDateFormat( dateFormat, Locale.US );
+		chosenDate = sdf.format( c.getTime() );
 		apptDate.setText( sdf.format( c.getTime() ) );
 		
 		String timeFormat = "hh:mm a";
 		SimpleDateFormat stf = new SimpleDateFormat( timeFormat, Locale.US );
+		chosenTime = stf.format( c.getTime() );
 		apptTime.setText( stf.format( c.getTime() ) );
 	}
 	
 	// Abstracted out functionality to avoid repetition
+	// Checkboxes -> Services
 	public void findViews() {
 		apptDate = (EditText)findViewById(R.id.apptDate);
 		apptTime = (EditText)findViewById(R.id.apptTime);
+		spStylistSelect = (Spinner)findViewById(R.id.spStylistSelect);
+		cbHairCut = (CheckBox)findViewById(R.id.cbHairCut);
+		cbPerm = (CheckBox)findViewById(R.id.cbPerm);
+		cbPartialHilites = (CheckBox)findViewById(R.id.cbPartialHilites);
+		cbHilites = (CheckBox)findViewById(R.id.cbHilites);
+		cbColor = (CheckBox)findViewById(R.id.cbColor);
+		cbPartialColor = (CheckBox)findViewById(R.id.cbPartialColor);
+		cbUpdo = (CheckBox)findViewById(R.id.cbUpdo);
+		cbBrazilianBlowout = (CheckBox)findViewById(R.id.cbBrazilianBlowout);
+		cbWax = (CheckBox)findViewById(R.id.cbWax);
+		cbExtensions = (CheckBox)findViewById(R.id.cbExtensions);		
 	}
+	
+	// Populate Spinner with <string-array name="stylists"> in res/values/strings.xml
+	public void populateSpinnerWithStylists() {
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.stylists, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spStylistSelect.setAdapter(adapter);
+		spStylistSelect.setOnItemSelectedListener(this);
+	}
+	
+	// Make temporary Toast to show customers what they chose
+	
+	public void tempToast() {
+		toastString = chosenStylist + " on " + chosenDate + " at " + chosenTime;
+		Toast toast = Toast.makeText(this, toastString, Toast.LENGTH_SHORT);
+		toast.show();
+	}
+	
+	public void scheduleAppt(View v) {
+		tempToast();
+	}
+	
+	// Implemented function from Spinner spStylistSelect.setOnItemSelectedListener
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		int stylistId = spStylistSelect.getSelectedItemPosition();
+		switch (stylistId) {
+		case 0:
+			chosenStylist = "Theresa";
+			break;
+		case 1:
+			chosenStylist = "Chrissy";
+			break;
+		case 2:
+			chosenStylist = "Stacy";
+			break;
+		case 3:
+			chosenStylist = "Sheri";
+			break;
+		case 4:
+			chosenStylist = "Krista";
+			break;
+		case 5:
+			chosenStylist = "Junae";
+			break;
+		}
+		
+	}
+
+	// Implemented function from Spinner spStylistSelect.setOnItemSelectedListener
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	  
 	// Create Method That Sends email (built in separate class? -> passing values? (maybe as integers and strings))
 	// Confirm with TOAST and redirect to main activity
 	
-	// Radio buttons -> Stylist
-	// Checkboxes -> Services
+	
 	
 }
 
